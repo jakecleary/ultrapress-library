a5be4001ef9c58eeb153cfcfa3507e7bf0f09481
jakecleary <shout@jakecleary.net>
2015-01-27 12:48:29 +0000

    Add small WP  wrapper

---
+++ src/Ultra/Global/Global.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ultra\Global;
+
+/**
+ * Wrapper class for interacting with Worpress' shitty global system.
+ */
+class Global
+{
+    /**
+     * Get the WP_Post object from the global instance.
+     *
+     * @return WP_Post
+     */
+    public static function post()
+    {
+        global $post;
+
+        return $post;
+    }
+
+    /**
+     * Get the WP_Query object from the global instance.
+     *
+     * @return WP_Query
+     */
+    public static function query()
+    {
+        global $wp_query;
+
+        return $wp_query;
+    }
+}
