c901991a93bc6c7cd2fcb7ad8f9024862f78f941
jakecleary <shout@jakecleary.net>
2015-01-27 12:23:39 +0000

    Add Asset class w/ interface

---
+++ src/Ultra/Asset/AssetInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ultra\Asset;
+
+class AssetInterface
+{
+    /**
+     * Get a stylesheet.
+     *
+     * @param string $file The name of the stylesheet
+     * @return string The full path to the specified stylesheet
+     */
+    public static function stylesheet($file);
+
+    /**
+     * Get a script.
+     *
+     * @param string $file The name of the script
+     * @return string The full path to the specified script
+     */
+    public static function script($file);
+
+    /**
+     * Get an image.
+     *
+     * @param string $file The name of the image
+     * @return string The full path to the specified image
+     */
+    public static function image($file);
+}
